d03433d1bdf4b112b61cf3eb30881005043b27d8
jreese8 <johnathanreese8@gmail.com>
2022-03-02 11:18:01 -0500

    Removed a comma

--- db/seeds.sql
+++ db/seeds.sql
@@ -18,7 +18,7 @@ INSERT INTO employees (first_name, last_name, role_id, manager_id)
 VALUES
   ('James', 'Fraser', 1, 2),
   ('Jack', 'London', 2, 1),
-  ('Robert', 'Bruce', 3, 2),
+  ('Robert', 'Bruce', 2, 2),
   ('Peter', 'Greenaway', 1, 2),
   ('Derek', 'Jarman', 2, 2),
   ('Paolo', 'Pasolini', 1, 2),
