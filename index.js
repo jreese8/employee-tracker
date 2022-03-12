d9104e8acf9554c002c75d757c346a4bc4834a07
jreese8 <johnathanreese8@gmail.com>
2022-03-11 20:07:00 -0500

    Post askbcs session

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const db = require('./db/connection');
 const inquirer = require('inquirer');
-const dbFunctions = require('./assets/dbFunctions');
 require("console.table");
 
 const Prompts = () => {
@@ -51,46 +50,193 @@ const Prompts = () => {
     //call a function depending on user choice
     switch (choice) {
       case "VIEW_DEPARTMENTS":
-        dbFunctions.viewDepartments();
+        viewDepartments();
         break;
       case "VIEW_ROLES":
-        dbFunctions.viewRoles();
+        viewRoles();
         break;
       case "VIEW_EMPLOYEES":
-        dbFunctions.viewEmployees();
+        viewEmployees();
         break;
       case "ADD_DEPARTMENT":
-        dbFunctions.addDepartment();
+        addDepartment();
         break;
       case "ADD_ROLE":
-        dbFunctions.addRole();
+        addRole();
         break;
       case "ADD_EMPLOYEE":
-        dbFunctions.addEmployee();
+        addEmployee();
         break;
       case "UPDATE_ROLE":
         updateRole();
         break;
       case "EXIT":
           //  close database connection and exit program
-        db.close()
+        db.close();
         break;
      }
     }
-  )}
-
+  )
+}
   db.connect(err => {
     if (err) throw err;
     console.log('Database connected.');
     Prompts(); 
   });
 
-
-const testFunc = () => {
-  console.log('this is a test function') //tests that module.exports is working
+  const viewDepartments = () => {
+    //Using database queries
+    db.query(
+        'SELECT * FROM departments', 
+        (err,res) => {
+            if(res) { 
+                console.log(res); //loop of all names of all departments
+                for (const object of res) { //object is current iteration of loop
+                    // each item in the res array is an object
+                    console.log(object.name)
+                }
+                // ask user prompts
+                Prompts();
+            }
+            else{ 
+                console.log(err);
+            }
+        }
+    )
 }
 
-  module.exports = {
-    Prompts:Prompts,
-    testFunc: testFunc
-  }
+const viewRoles = () => {
+    db.query(
+        'SELECT * FROM roles', 
+        (err,res) => {
+            if(res) { 
+                console.log(res); //loop of all names of all roles
+                for (const object of res) { //object is current iteration of loop
+                    // each item in the res array is an object
+                    console.log(object.name)
+                }
+                // ask user prompts
+                Prompts();
+            }
+            else{ 
+                console.log(err);
+            }
+        }
+    )
+}
+
+const viewEmployees = () => {
+    db.query(
+        'SELECT * FROM employees', 
+        (err,res) => {
+            if(res) { 
+                console.log(res); //loop of all names of all roles
+                for (const object of res) { //object is current iteration of loop
+                    // each item in the res array is an object
+                    console.log(object.name)
+                }
+                // ask user prompts
+                Prompts();
+            }
+            else{ 
+                console.log(err);
+            }
+        }
+    )
+}
+
+const addDepartment = () => {
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "addDept",
+            message: "Add a department.",
+            validate: deptInput => {
+                if (deptInput) {
+                    return true;
+                } else {
+                    console.log ("Please add a department.");
+                    return false; 
+                }
+              }
+            }    
+            
+        ]).then((res) => {
+            const addData = `INSERT INTO departments (name) VALUES ('${res.addDept}')`; //inserting department name as template literal
+            db.query(addData);
+          }).then(Prompts());
+        }
+
+const addRole = () => { //Add more to this
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "addRole",
+            message: "Add a role.",
+            validate: roleInput => {
+                if (roleInput) {
+                    return true;
+                } else {
+                    console.log ("Please add a role.");
+                    return false; 
+                }
+              }
+            }    
+            
+        ]).then((res) => {
+            const addData = `INSERT INTO roles (name) VALUES ('${res.addRole}')`;
+            db.query(addData);
+          }).then(Prompts());
+        }
+
+const addEmployee = () => {
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "addEmployee",
+            message: "Add an employee.",
+            validate: employeeInput => {
+                if (employeeInput) {
+                    return true;
+                } else {
+                    console.log ("Please add an employee.");
+                    return false; 
+                }
+              }
+            }    
+            
+        ]).then((res) => {
+            const addData = `INSERT INTO employees (name) VALUES ('${res.addEmployee}')`;
+            db.query(addData)
+            }).then(Prompts());
+        }
+
+const updateRole = () => { //Doesnt work
+    const selectEmployees = 'SELECT * FROM employees';
+    db.query(selectEmployees, (err, res) => {
+      if(err){console.error(err)}
+      if(res){console.table(res)}
+
+    const selectRoles = 'SELECT * FROM roles';
+    db.query(selectRoles, (err, res) => {
+      if(err){console.error(err)}
+      if(res){console.table(res)}
+
+    return inquirer.prompt([
+        {
+          type: "input",
+          message: "Which employee like to update?",
+          name: "employeeUpdate"
+        },
+        {
+          type: "input",
+          message: "What is their new role ID number?",
+          name: "roleUpdate"
+        }
+    ]).then((res) => {
+        const dataUpdate = `UPDATE employees SET role_id = ${res.updateRole} WHERE id = ${res.employeeUpdate}`;
+        db.query(dataUpdate);
+        }).then(Prompts());
+    })
+})
+}
